0bc9fc0275f594def322a83c9d90c61edbe93956
Tim <>
2019-11-22 09:01:17 +1100

    deploy

---
+++ v2frontend/doc/precache-manifest.b89025490270f7e3232f1096febd8048.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90adf314cd188d2d26d8add34a811e50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea6c4ff14ffda625b6a5",
+    "url": "/static/css/2.aa8bac49.chunk.css"
+  },
+  {
+    "revision": "a9b7557d9f9413863ce1",
+    "url": "/static/css/main.21d29323.chunk.css"
+  },
+  {
+    "revision": "ea6c4ff14ffda625b6a5",
+    "url": "/static/js/2.e0b08b35.chunk.js"
+  },
+  {
+    "revision": "a9b7557d9f9413863ce1",
+    "url": "/static/js/main.c52aa9fd.chunk.js"
+  },
+  {
+    "revision": "dea3119fd97b4e61b18a",
+    "url": "/static/js/runtime-main.cc8b9818.js"
+  },
+  {
+    "revision": "6f2fb8c11e75bebeb40b2a099dbe50e6",
+    "url": "/static/media/1.6f2fb8c1.jpeg"
+  },
+  {
+    "revision": "da9f6711debf9aba0a4668e4e516c151",
+    "url": "/static/media/2.da9f6711.jpeg"
+  },
+  {
+    "revision": "7eee0cd957eae43ccd3727ea4bf6cb17",
+    "url": "/static/media/3.7eee0cd9.jpeg"
+  },
+  {
+    "revision": "583f9c69fdd5dab10eae91f16ffc9df0",
+    "url": "/static/media/logo-default.583f9c69.png"
+  }
+]);

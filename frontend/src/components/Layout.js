a20751ea9e6ef165523039aba357a4a3f431ad85
cedriclemercier <cedric.lemercier@outlook.com>
2019-11-24 15:10:09 +1100

    Small dashboard design changes

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -1,76 +1,102 @@
-import logo from '../assets/logo-default.png';
-import classes from './Layout.module.css';
-import { Layout, Menu, Icon, Button} from 'antd';
-import React from "react"
-import "antd/dist/antd.css"
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import logo from "../assets/logo-default.png";
+import { Layout, Menu, Icon, Button } from "antd";
+import React from "react";
+import "antd/dist/antd.css";
+import "./Layout.css";
+// import './style.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 const { Header, Sider, Content } = Layout;
-
 
 class SideLayout extends React.Component {
   state = {
-    collapsed: false,
+    collapsed: false
   };
 
   toggle = () => {
     this.setState({
-      collapsed: !this.state.collapsed,
+      collapsed: !this.state.collapsed
     });
   };
 
   render() {
     return (
       <Layout>
-        <link href="https://api.mapbox.com/mapbox-gl-js/v0.51.0/mapbox-gl.css" rel="stylesheet" />
-        <Sider trigger={null} collapsible collapsed={this.state.collapsed} style={{height:"100vh", backgroundColor: '#fff', borderRight: '2px solid #EAE9EE'}}>
-          <div style={{marginRight:"auto",marginLeft:"auto",display:"block",width:"50%"}}><img src={logo} style={{maxHeight:"50px", margin:"16px"}}/></div>
-          <Menu theme="light" mode="inline" defaultSelectedKeys={['0']} style={{backgroundColor: '#fff', fontWeight: '600', color: '#9A99B0', borderRight: '0'}}>
-          
-          <Menu.Item key="2">
-             <Link to="/dashboard">
-              <Icon type="dashboard" />
-              <span> Dashboard </span>
+        <link
+          href="https://api.mapbox.com/mapbox-gl-js/v0.51.0/mapbox-gl.css"
+          rel="stylesheet"
+        />
+        <Sider
+          trigger={null}
+          collapsible
+          collapsed={this.state.collapsed}
+          style={{
+            height: "100vh",
+            backgroundColor: "#fff",
+            borderRight: "2px solid #EAE9EE"
+          }}
+        >
+          <div
+            style={{
+              marginRight: "auto",
+              marginLeft: "auto",
+              display: "block",
+              width: "50%"
+            }}
+          >
+            <img src={logo} style={{ maxHeight: "50px", margin: "16px 16px 60px 16px" }} />
+          </div>
+          <Menu
+            theme="light"
+            mode="inline"
+            defaultSelectedKeys={["0"]}
+            style={{
+              backgroundColor: "#fff",
+              fontWeight: "600",
+              color: "#9A99B0",
+              borderRight: "0"
+            }}
+          >
+            <Menu.Item key="2">
+              <Link to="/dashboard">
+                <Icon type="dashboard" />
+                <span> Dashboard </span>
               </Link>
             </Menu.Item>
             <Menu.Item key="3">
-            <Link to="/team">
-              <Icon type="team" />
-              <span> Team </span>
-            </Link>
+              <Link to="/team">
+                <Icon type="team" />
+                <span> Team </span>
+              </Link>
             </Menu.Item>
 
-            <div style={{marginTop:"70vh", marginLeft:"30px", position:"absolute",  padding:"15px", borderStyle:"dotted"}}>
-            <Link to="/intro">
+            <div className="btn-read" >
+              <Link to="/intro">
+                <Icon type="read" />
+                <span style={{ paddingLeft: "10px" }}>Read Story</span>
+              </Link>
 
-            <Icon type="read" />
-            <span style={{paddingLeft:"10px"}}>Read Story</span>
-            </Link>
             </div>
-
-
           </Menu>
         </Sider>
+
         <Layout>
-          <div style={{height:"100vh", padding:"10px"}}>
-          <Content
-            style={{
-              padding: 24,
-              background: '#fff',
-              Height: "100vh",
-            }}
-          >
-            {this.props.content}
-          </Content>
+          <div style={{ height: "100vh", padding: "10px" }}>
+            <Content
+              style={{
+                padding: 24,
+                background: "#fff",
+                Height: "100vh",
+                borderRadius: 20
+              }}
+            >
+              {this.props.content}
+            </Content>
           </div>
         </Layout>
+
       </Layout>
     );
   }
 }
 
-export default SideLayout
+export default SideLayout;
